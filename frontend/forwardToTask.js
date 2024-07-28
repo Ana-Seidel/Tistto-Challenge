21a8bed08a181183b483af18e0beaae475e518cc
Ana-Seidel <anaafsw1@gmail.com>
2024-07-28 09:48:33 -0300

    background change

--- frontend/forwardToTask.js
+++ frontend/forwardToTask.js
@@ -9,3 +9,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('form');
     form.addEventListener('submit', handleFormSubmit);
 });
+
+// Função para ter o spinner para carregar a página
+document.querySelectorAll('.link').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        document.getElementById('spinner').style.display = 'block';
+        setTimeout(() => {
+            window.location.href = this.href;
+        }, 1000);
+    });
+});
