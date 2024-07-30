f6c1fe165a5921bcaaa4c37cb6b6dbc8f9c993fc
Ana-Seidel <anaafsw1@gmail.com>
2024-07-30 16:44:38 -0300

    additional functionality of the todo list and changing the background

--- frontend/toDoList.js
+++ frontend/toDoList.js
@@ -1,9 +1,51 @@
-function newTask()
-{
-    let input = document.getElementById("taskbar")
-    //Garantir que foi escrito algo
-    if(!input.value)
-    {
-        alert("É preciso escrever algo para adicionar na sua lista")
-    }
+const button = document.querySelector("#add-task")
+const input = document.querySelector("#taskbar")
+const allList = document.querySelector(".list-task")
+
+let itensNaLista = []
+
+
+function addTarefa() {
+    itensNaLista.push({
+        tarefa: input.value,
+        concluida: false
+    })
+    
+    mostrarTarefa()
+    //Limpa a taskbar
+    input.value = ""
 }
+
+function mostrarTarefa(){
+    let novaLi = ""
+
+    itensNaLista.forEach( (item, index) => {
+        novaLi = novaLi + `
+        <li class="task ${item.concluida && "done"}">
+            <img src="./assets/check-circle.png" onclick="checkTask(${index})">
+            <p>${item.tarefa}</p>
+            <img src="./assets/trash3-fill.png" onclick="deleteItem(${index})">
+        </li>
+        `
+ })
+
+ allList.innerHTML = novaLi
+
+}
+
+function checkTask(index){
+    itensNaLista[index].concluida = !itensNaLista[index].concluida
+
+    mostrarTarefa()
+}
+
+function deleteItem(index){
+
+    itensNaLista.splice(index, 1)
+
+    mostrarTarefa()
+
+}
+
+// Quando clica no botão para criar
+button.addEventListener("click", addTarefa)
