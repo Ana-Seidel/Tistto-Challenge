66c2a47fdd5d11ebb3c8fb07dcfad7aadc440f58
Ana-Seidel <anaafsw1@gmail.com>
2024-07-31 18:51:49 -0300

    (backend): created User model

--- todo_project/tasks/models.py
+++ todo_project/tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)  # Em produção, você deve usar um hash de senha
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.name} {self.last_name}"
